d583ba4ad777d9d34600187aa0df5dd493e25774
Prashant Sahni <prashant.sahni5@gmail.com>
2024-04-30 16:55:00 +0530

    Fix routing issue

---
+++ src/components/Products.jsx
@@ -0,0 +1,15 @@
+const Products = () => {
+    // const [products, setProducts] = useState([]);
+
+    // const getProducts = async () => {
+
+    // }
+
+    return (
+        <section>
+            Hello Products
+        </section>
+    )
+}
+
+export default Products;
